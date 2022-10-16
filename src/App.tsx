a92c34d2e50af98b24ecde24111d777303cd52e5
= <LHP6.arazzok@gmail.com>
2022-10-16 22:51:06 +0200

    add Authcontext and app rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useContext } from "react"
 import "rsuite/dist/rsuite.min.css"
-import CustomProvider from "rsuite/esm/CustomProvider/CustomProvider";
+import CustomProvider from "rsuite/esm/CustomProvider/CustomProvider"
+import AuthContext from "./context/AuthProvider"
 import './App.scss'
-import Login from './components/Login';
+import Login from './components/Login'
+import Hub from "./components/Hub"
 
 const App = () => {
+  const { auth } = useContext(AuthContext)
+
   return (
     <CustomProvider theme="dark">
-      <Login />
+      {auth.accessToken ?
+        <Hub />
+      :
+        <Login />}
     </CustomProvider>
   );
 }
--- src/Index.tsx
+++ src/Index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './context/AuthProvider'
 import App from './App'
 
 const root = createRoot(document.getElementById('root')!)
 
 root.render(
-  <App />
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 )
