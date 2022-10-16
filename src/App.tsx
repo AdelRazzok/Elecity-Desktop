53eb7290b85f9d483a04e28c2151910162923eee
habibHadjar <lhp6.hhadjar@gmail.com>
2022-10-16 22:46:12 +0200

    boiler

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import './App.scss'
 import React from "react";
+import "rsuite/dist/rsuite.min.css"
+import CustomProvider from "rsuite/esm/CustomProvider/CustomProvider";
+import './App.scss'
+import Login from './components/Login';
 
 const App = () => {
   return (
-    <div>App</div>
+    <CustomProvider theme="dark">
+      <Login />
+    </CustomProvider>
   );
 }
 export default App
--- src/components/Hub/Hub.tsx
+++ src/components/Hub/Hub.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './Hub.scss'
+import { Container, Header, Content, Footer, Navbar, Nav } from 'rsuite';
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import CogIcon from '@rsuite/icons/legacy/Cog';
 
 const Hub = () => {
   return (
-    <div>Hub</div>
+    <div className="show-fake-browser navbar-page">
+    <Container>
+      <Header>
+        <Navbar appearance="inverse">
+          <Navbar.Brand>
+            <a style={{ color: '#fff' }}>Brand</a>
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
+            <Nav.Item>News</Nav.Item>
+            <Nav.Item>Products</Nav.Item>
+            <Nav.Menu title="About">
+              <Nav.Item>Company</Nav.Item>
+              <Nav.Item>Team</Nav.Item>
+              <Nav.Item>Contact</Nav.Item>
+            </Nav.Menu>
+          </Nav>
+          <Nav pullRight>
+            <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+          </Nav>
+        </Navbar>
+      </Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Container>
+  </div>
   )
 }
 
