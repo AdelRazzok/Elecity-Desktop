85005707ce62ba8eedbfbaea62df59ff51523d81
habibHadjar <lhp6.hhadjar@gmail.com>
2022-10-17 12:09:02 +0200

    scss

--- src/components/Hub/Hub.tsx
+++ src/components/Hub/Hub.tsx
@@ -11,16 +11,17 @@ const Hub = () => {
       <Header>
         <Navbar appearance="inverse">
           <Navbar.Brand>
-            <a style={{ color: '#fff' }}>Brand</a>
+            <a style={{ color: '#fff' }}>Elecity</a>
           </Navbar.Brand>
           <Nav>
-            <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
-            <Nav.Item>News</Nav.Item>
-            <Nav.Item>Products</Nav.Item>
-            <Nav.Menu title="About">
-              <Nav.Item>Company</Nav.Item>
-              <Nav.Item>Team</Nav.Item>
-              <Nav.Item>Contact</Nav.Item>
+            <Nav.Item icon={<HomeIcon />}>Accueil</Nav.Item>
+            <Nav.Item>Parcs</Nav.Item>
+            <Nav.Item>Voitures</Nav.Item>
+            <Nav.Item>Offres</Nav.Item>
+            <Nav.Menu title="Management">
+              <Nav.Item>Clients</Nav.Item>
+              <Nav.Item>Collaborateurs</Nav.Item>
+              {/* <Nav.Item>blabla</Nav.Item> */}
             </Nav.Menu>
           </Nav>
           <Nav pullRight>
